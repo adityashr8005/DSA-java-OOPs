cf5e373b615694320795b88353a64f42cb046b73
adityashr8005 <124335796+adityashr8005@users.noreply.github.com>
2025-02-12 16:40:25 +0530

    Update Rotate_Array1.java

--- DSA/src/Leet/OO2/Rotate_Array1.java
+++ DSA/src/Leet/OO2/Rotate_Array1.java
@@ -4,21 +4,22 @@ import java.util.Arrays;
 
 public class Rotate_Array1 {
     public static void main(String[] args) {
-        int[] arr = {-1};
+        int[] arr = {1,2,3,4,5,6};
         int k=2;
         rotate(arr,k);
     }
     static void rotate(int[] nums, int k){
         int n=nums.length;
-        k=k%n;
-        reverse(nums,0,n-1);
-        reverse(nums,0,k-1);
-        reverse(nums,k,n-1);
+        k=k%n; //In case k is greater.
+        reverse(nums,0,n-1); //Reverse the array
+        reverse(nums,0,k-1); //arrange the shifted element to right position
+        reverse(nums,k,n-1); //arrange the remaining element to right position
 
         System.out.println(Arrays.toString(nums));
     }
     static void reverse(int[] nums,int s,int e){
         while (s<e){
+            //Swaping the elements
             int temp = nums[s];
             nums[s]=nums[e];
             nums[e]=temp;
