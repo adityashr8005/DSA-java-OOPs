75a044394612fc671b58d335e6af47ba0fbf3d4f
adityashr8005 <124335796+adityashr8005@users.noreply.github.com>
2025-02-12 17:47:57 +0530

    Revert "Update BubbleSort.java"

--- DSA/src/DSA_Kunal/Algorithm/BubbleSort.java
+++ DSA/src/DSA_Kunal/Algorithm/BubbleSort.java
@@ -8,7 +8,6 @@ package DSA_Kunal.Algorithm;
 import java.util.Arrays;
 
 public class BubbleSort {
-    //main function.
     public static void main(String[] args) {
 
         int[] arr = {3,4,5,2,1};
