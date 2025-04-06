7daecf9f483f3f06c40bb358bdf4f5b0ec3edad3
Aaditya <shr.aadi000@gmail.com>
2025-04-06 22:33:44 +0530

    6th April question solved

---
+++ DSA/src/Leet/April/LargestDivSubset.java
@@ -0,0 +1,53 @@
+package Leet.April;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LargestDivSubset {
+    public static void main(String[] args) {
+        int[] arr = {};
+        System.out.println(largestDivisibleSubset(arr));
+    }
+    static List<Integer> largestDivisibleSubset(int[] nums){
+        List<Integer> list = new ArrayList<>();
+
+        if (nums.length==0){
+            return list;
+        }
+        if (nums.length==1){
+            list.add(nums[0]);
+            return list;
+        }
+
+        Arrays.sort(nums);
+        //Declaration of an array of size nums.length;
+        int[] arr = new int[nums.length];
+        //Filling all element sof arr with 1.
+        Arrays.fill(arr, 1);
+
+        int max=0;
+        //Nested loop to find max number of elements for the largest Divisible Subset
+        for (int i=1; i<nums.length; i++){
+            for (int j=0; j<i; j++){
+                if (nums[i]%nums[j]==0 && 1+arr[j]>arr[i]){
+                    arr[i]=1+arr[j];
+                }
+                if (max<arr[i]){
+                    max=arr[i];
+                }
+            }
+        }
+
+        int temp=-1;
+        //Loop to add an element to a list of maximum subset.
+        for (int i=arr.length-1; i>=0; i--){
+            if ((arr[i] == max) && ((((temp % nums[i]) == 0) || temp == -1))){
+                list.add(nums[i]);
+                max -= 1;
+                temp = nums[i];
+            }
+        }
+        return list;
+    }
+}
