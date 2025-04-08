2ca0f3fcb9a9b9ae073ac3145fb5bb25fdd3755c
Aaditya <shr.aadi000@gmail.com>
2025-04-08 22:39:21 +0530

    modified

--- DSA/src/Leet/OO1/test1.java
+++ DSA/src/Leet/OO1/test1.java
@@ -1,19 +1,22 @@
 package Leet.OO1;
 
+import java.util.Arrays;
+
 public class test1 {
     public static void main(String[] args) {
-        int[] nums={2,4,5,3};
+        int[] nums={2,1,5,6};
         int target=8;
-        twoSum(nums,target);
+        System.out.println(Arrays.toString(twoSum(nums,target)));
 
     }
 
     static int[] twoSum(int[] nums,int target){
 
         for (int i=0; i< nums.length; i++){
-            int j=i+1;
-            if (nums[i]+nums[j]==target){
-                return new int[] {i,j};
+            for(int j = i + 1; j < nums.length; j++){
+                if (nums[i]+nums[j]==target){
+                    return new int[] {i,j};
+                }
             }
         }
         return null;
