69489c3c108d66fc8f4f07c3cdb94bcbfccc0afb
aadi-Code-7 <shr.aadi000@gmail.com>
2026-03-10 15:57:07 +0530

    10th march qn. solved

---
+++ DSA/src/Leet/Year2026/March/MinimumSwaps.java
@@ -0,0 +1,75 @@
+package Leet.Year2026.March;
+
+
+/*
+Given an n x n binary grid, in one step you can choose two adjacent rows of the grid and swap them.
+A grid is said to be valid if all the cells above the main diagonal are zeros.
+Return the minimum number of steps needed to make the grid valid, or -1 if the grid cannot be valid.
+The main diagonal of a grid is the diagonal that starts at cell (1, 1) and ends at cell (n, n).
+
+Example 1:
+Input: grid = [[0,0,1],[1,1,0],[1,0,0]]
+Output: 3
+
+Example 2:
+Input: grid = [[0,1,1,0],[0,1,1,0],[0,1,1,0],[0,1,1,0]]
+Output: -1
+Explanation: All rows are similar, swaps have no effect on the grid.
+
+Example 3:
+Input: grid = [[1,0,0],[1,1,0],[1,1,1]]
+Output: 0
+
+Constraints:
+n == grid.length == grid[i].length
+1 <= n <= 200
+grid[i][j] is either 0 or 1
+ */
+public class MinimumSwaps {
+    public static int minSwaps(int[][] grid){
+
+        int n = grid.length;
+        int[] zeroes = new int[n];
+
+        for (int i=0; i<n; i++){
+            int j = n-1;
+            int count = 0;
+            while (j>=0 &&  grid[i][j] == 0){
+                count++;
+                j--;
+            }
+            zeroes[i] = count;
+        }
+
+        int flip = 0;
+        for (int i = 0; i<n; i++){
+            int requiredZeroes = n-i-1;
+            int j = i;
+            while (j<n && zeroes[j] < requiredZeroes){
+                j++;
+            }
+            if (j == n) return -1;
+
+            while (j>i){
+                int temp = zeroes[j];
+                zeroes[j] = zeroes[j-1];
+                zeroes[j-1] = temp;
+                flip++;
+                j--;
+            }
+        }
+        return flip;
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {
+                {0,0,0},
+                {1,1,0},
+                {1,1,1}
+        };
+        int ans = minSwaps(grid);
+        System.out.println(ans);
+    }
+}
+
+
